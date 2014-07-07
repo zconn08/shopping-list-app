9b5e072c26d4be3e997674bad31603964268cd65
zconn08 <zconn@Zacharys-MacBook-Pro.local>
2014-07-06 20:14:03 -0700

    Draft 2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
 $(document).ready(function(){
+	//Add an item
+	$(".add_item_button").click(function(){
+		var textbox = document.getElementById("add_item_input");
+		var textval = textbox.value;
+		if (textval === "") {
+			alert("Enter text to add your item");
+		} else {
+			$('<li class="item"></li>').appendTo(".list_container").html('<div class="checkbox unchecked"></div><span class="item_text">'+ textval + '</span><div class="delete"></div>');
+		}
+		document.getElementById('add_item_input').value = '';
+	});
+
+	//Enter key to add item
+	$(document).keyup(function(e){
+		if (e.which==13) {
+			$(".add_item_button").click();
+		}
+	});
+
 	//Remove an item
 	$(".list_container").on("click",".delete",function(e){
-		e.preventDefault();
 		$(this).parent().remove();
 	});
+
 	//Change item to completed
 	$(".list_container").on("click","li",function(e){
-		e.preventDefault();
 		$(this).toggleClass("completed");
 		$(this).children(".checkbox").toggleClass("checked");
 	});
+
 	//Delete button appears when hovering over an item
 	$(".list_container").on("mouseenter","li",function(){
 		$(this).children(".delete").toggleClass("show");
@@ -17,4 +36,7 @@ $(document).ready(function(){
 	$(".list_container").on("mouseleave","li",function(){
 		$(this).children(".delete").toggleClass("show");
 	});
+
+	//Make items sortable
+	$(".list_container").sortable();
 });
