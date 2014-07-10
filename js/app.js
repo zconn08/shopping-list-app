43b9b994210824302501f5466d7e2f51d92d4a06
zconn08 <zconn@unknowna8bbcf21d658.gateway.pace.com>
2014-07-09 21:34:42 -0700

    Final Version

--- js/app.js
+++ js/app.js
@@ -3,10 +3,8 @@ $(document).ready(function(){
 	$(".add_item_button").click(function(){
 		var textbox = document.getElementById("add_item_input");
 		var textval = textbox.value;
-		if (textval === "") {
-			alert("Please enter the name of an item to add it to your shopping list");
-		} else {
-			$('<li class="item"></li>').appendTo(".list_container").html('<div class="checkbox unchecked"></div><span class="item_text">'+ textval + '</span><div class="delete"></div>');
+		if (textval !== "") {
+			$('<li class="item"></li>').appendTo("#list_container").html('<div class="checkbox unchecked"></div><span class="item_text">'+ textval + '</span><div class="delete"></div>');
 		}
 		document.getElementById('add_item_input').value = '';
 	});
@@ -19,24 +17,24 @@ $(document).ready(function(){
 	});
 
 	//Remove an item
-	$(".list_container").on("click",".delete",function(e){
+	$("#list_container").on("click",".delete",function(e){
 		$(this).parent().remove();
 	});
 
 	//Change item to completed
-	$(".list_container").on("click","li",function(e){
+	$("#list_container").on("click","li",function(e){
 		$(this).toggleClass("completed");
 		$(this).children(".checkbox").toggleClass("checked");
 	});
 
 	//Delete button appears when hovering over an item
-	$(".list_container").on("mouseenter","li",function(){
+	$("#list_container").on("mouseenter","li",function(){
 		$(this).children(".delete").toggleClass("show");
 	});
-	$(".list_container").on("mouseleave","li",function(){
+	$("#list_container").on("mouseleave","li",function(){
 		$(this).children(".delete").toggleClass("show");
 	});
 
 	//Make items sortable
-	$(".list_container").sortable();
+	$("#list_container").sortable({ axis: "y" });
 });
